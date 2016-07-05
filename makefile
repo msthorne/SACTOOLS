9468d3b61048b4d7d8c9362e7238fd026e381d0e
Michael Thorne <u0597445@fats.geophys.utah.edu>
2016-07-05 07:39:26 -0600

    Added the 'xy2sac.f90' program to the repository. Also edited the makefile to compile xy2sac.f90

--- makefile
+++ makefile
@@ -7,7 +7,7 @@ RM=/bin/rm -f
 BINDIR=./bin                 
 
 
-all : modules sac2xy copy
+all : modules sac2xy xy2sac 
 
 #Compile modules.
 modules : mod_sac_io.f90
@@ -16,15 +16,18 @@ modules : mod_sac_io.f90
 #Compile Source-code and link.
 sac2xy : sac2xy.f90 modules
 	$(F90) $(FFLAGS) sac2xy.f90 -o sac2xy ./mod_sac_io.o
+	mv sac2xy $(BINDIR)
+
+xy2sac : xy2sac.f90 modules
+	$(F90) $(FFLAGS) xy2sac.f90 -o xy2sac ./mod_sac_io.o
+	mv xy2sac $(BINDIR)
 
 
 #Copy executable to appropriate directories.
 #main : mod_sac_io.o sac2xy
 #	cp sac2xy $(BINDIR)
 
-copy :
-	cp sac2xy ${BINDIR}
-
 clean :
-	  $(RM) sac2xy mod_sac_io.o sac_i_o.mod
+	  $(RM) mod_sac_io.o sac_i_o.mod
+
 
---
+++ xy2sac.f90
@@ -0,0 +1,112 @@
+PROGRAM xysac
+!Convert xy table to SAC file
+!by Michael Thorne
+!USE F90_UNIX_ENV
+USE sac_i_o
+IMPLICIT NONE
+REAL(KIND=4), DIMENSION(:), ALLOCATABLE :: amps, times, dummy
+REAL(KIND=4) :: deltaa
+INTEGER(KIND=4)    :: NN, ios, J, NR
+INTEGER(KIND=4), PARAMETER :: maxrecs = 10000000
+CHARACTER(LEN=112) :: xytable, osacfile, ofile
+CHARACTER(LEN=1) :: junk
+
+
+!    --  R E A D  U S E R  I N P U T  --
+!:=====:=====:=====:=====:=====:=====:=====:=====:=====:=====:=====:=====:=====:!
+NN = IARGC()
+IF (NN < 2) THEN
+  write(*,'(a)') "usage:  xy2sac xytable sacfile"
+  write(*,'(a)') "        xytable - input xy file"
+  write(*,'(a)') "        sacfile - name of output sac file"
+  STOP
+ENDIF
+
+CALL GETARG(1, xytable)
+OPEN(UNIT=1,FILE=xytable,STATUS='OLD',IOSTAT=ios)
+IF (ios > 0) THEN
+  write(*,*) "ERROR - Input file: '", TRIM(adjustl(xytable)), "' does not exist ..."
+  CLOSE(1)
+  STOP
+ENDIF
+CLOSE(1) 
+
+CALL GETARG(2,osacfile)
+
+write(*,*) "Writing XY file: '", TRIM(adjustl(xytable)), "' to SAC file: '", &
+           & TRIM(adjustl(osacfile)), "' ..."
+
+
+!    --  R E A D   I N P U T  X-Y  T A B L E  --
+!:=====:=====:=====:=====:=====:=====:=====:=====:=====:=====:=====:=====:=====:!
+
+!Determine total number of lines in file
+NR = 0
+OPEN(UNIT=1,FILE=xytable)
+DO J=1,maxrecs
+  READ(1,*,IOSTAT=ios) junk
+  IF (ios /= 0) EXIT
+  IF (J == maxrecs) THEN
+    write(*,*) "Error:  Maximum number of records exceeded..."
+    write(*,*) "  Exiting now!"
+    STOP
+  ENDIF
+  NR = NR + 1
+ENDDO
+REWIND(1)
+
+!Allocate data
+ALLOCATE(times(NR))
+ALLOCATE(amps(NR))
+
+!Now read in data
+DO J=1,NR
+  READ(1,*) times(J), amps(J)
+ENDDO
+CLOSE(1)
+
+
+!    --  W R I T E  O U T P U T  S A C F I L E  --
+!:=====:=====:=====:=====:=====:=====:=====:=====:=====:=====:=====:=====:=====:!
+
+!Initialize sac file
+ofile = 'temp'
+CALL initsac(ofile,delta,depmin,depmax,scale,odelta,b,e,o,a,internal1,       &
+t0,t1,t2,t3,t4,t5,t6,t7,t8,t9,f,resp0,resp1,resp2,resp3,resp4,resp5,resp6,   &
+resp7,resp8,resp9,stla,stlo,stel,stdp,evla,evlo,evel,evdp,mag,user0,user1,   &
+user2,user3,user4,user5,user6,user7,user8,user9,dist,az,baz,gcarc,internal2, &
+internal3,depmen,cmpaz,cmpinc,xminimum,xmaximum,yminimum,ymaximum,unused1,   &
+unused2,unused3,unused4,unused5,unused6,unused7,nzyear,nzjday,nzhour,nzmin,  &
+nzsec,nzmsec,nvhdr,norid,nevid,npts,internal4,nwfid,nxsize,nysize,unused8,   &
+iftype,idep,iztype,unused9,iinst,istreg,ievreg,ievtyp,iqual,isynth,imagtyp,  &
+imagsrc,unused10,unused11,unused12,unused13,unused14,unused15,unused16,      &
+unused17,leven,lpspol,lovrok,lcalda,unused18,kevnm,kstnm,khole,ko,ka,kt0,kt1,&
+kt2,kt3,kt4,kt5,kt6,kt7,kt8,kt9,kf,kuser0,kuser1,kuser2,kcmpnm,knetwk,kdatrd,&
+kinst,dummy)
+
+!find delta by simply difference the first two time steps
+deltaa = times(2) - times(1)
+depmin = MINVAL(amps)
+depmax = MAXVAL(amps)
+b = times(1)
+npts = NR
+kuser0 = 'xy2sac'
+
+!now write out sac file
+CALL wbsac(osacfile,deltaa,depmin,depmax,scale,odelta,b,e,o,a,internal1,     &
+t0,t1,t2,t3,t4,t5,t6,t7,t8,t9,f,resp0,resp1,resp2,resp3,resp4,resp5,resp6,   &
+resp7,resp8,resp9,stla,stlo,stel,stdp,evla,evlo,evel,evdp,mag,user0,user1,   &
+user2,user3,user4,user5,user6,user7,user8,user9,dist,az,baz,gcarc,internal2, &
+internal3,depmen,cmpaz,cmpinc,xminimum,xmaximum,yminimum,ymaximum,unused1,   &
+unused2,unused3,unused4,unused5,unused6,unused7,nzyear,nzjday,nzhour,nzmin,  &
+nzsec,nzmsec,nvhdr,norid,nevid,npts,internal4,nwfid,nxsize,nysize,unused8,   &
+iftype,idep,iztype,unused9,iinst,istreg,ievreg,ievtyp,iqual,isynth,imagtyp,  &
+imagsrc,unused10,unused11,unused12,unused13,unused14,unused15,unused16,      &
+unused17,leven,lpspol,lovrok,lcalda,unused18,kevnm,kstnm,khole,ko,ka,kt0,kt1,&
+kt2,kt3,kt4,kt5,kt6,kt7,kt8,kt9,kf,kuser0,kuser1,kuser2,kcmpnm,knetwk,kdatrd,&
+kinst,amps)
+
+
+
+
+END PROGRAM xysac
