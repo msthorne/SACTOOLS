b5c36b657ebc6e6f12e01f0657f4a8b05dc8a3e4
Michael Thorne <michael.thorne@utah.edu>
2016-07-22 04:27:12 -0600

    Added examples for 'addnoise.f90'

--- addnoise.f90
+++ addnoise.f90
@@ -123,7 +123,7 @@ DO JK=1,NR
     auto(J)=exp(-(xgrid(J)**2/corner))
   END FORALL
 
-  !Write out sacfile
+  !Write out autocorrelation function to an ascii file
   IF (JK == 1) THEN
 
    OPEN(UNIT=33,FILE='auto_gauss.xy')
@@ -173,24 +173,42 @@ DO JK=1,NR
   auto = auto - ave
   auto = (auto/sdev)*rms
 
+  !Write out noise file for first instance for checking
+  IF (JK == 1) THEN
+    ofile = 'noise.sac'
+
+    CALL wbsac(ofile,delta,depmin,depmax,scale,odelta,b,e,o,a,internal1,         &
+    t0,t1,t2,t3,t4,t5,t6,t7,t8,t9,f,resp0,resp1,resp2,resp3,resp4,resp5,resp6,   &
+    resp7,resp8,resp9,stla,stlo,stel,stdp,evla,evlo,evel,evdp,mag,user0,user1,   &
+    user2,user3,user4,user5,user6,user7,user8,user9,dist,az,baz,gcarc,internal2, &
+    internal3,depmen,cmpaz,cmpinc,xminimum,xmaximum,yminimum,ymaximum,unused1,   &
+    unused2,unused3,unused4,unused5,unused6,unused7,nzyear,nzjday,nzhour,nzmin,  &
+    nzsec,nzmsec,nvhdr,norid,nevid,npts,internal4,nwfid,nxsize,nysize,unused8,   &
+    iftype,idep,iztype,unused9,iinst,istreg,ievreg,ievtyp,iqual,isynth,imagtyp,  &
+    imagsrc,unused10,unused11,unused12,unused13,unused14,unused15,unused16,      &
+    unused17,leven,lpspol,lovrok,lcalda,unused18,kevnm,kstnm,khole,ko,ka,kt0,kt1,&
+    kt2,kt3,kt4,kt5,kt6,kt7,kt8,kt9,kf,kuser0,kuser1,kuser2,kcmpnm,knetwk,kdatrd,&
+    kinst,auto)
+  ENDIF
+
   !Add to SAC file
   sacdata(1:npts) = sacdata(1:npts) + auto(1:npts)
 
   !Write out sacfile
   ofile = 'noise_'//TRIM(ADJUSTL(file1))
 
-CALL wbsac(ofile,delta,depmin,depmax,scale,odelta,b,e,o,a,internal1,         &
-t0,t1,t2,t3,t4,t5,t6,t7,t8,t9,f,resp0,resp1,resp2,resp3,resp4,resp5,resp6,   &
-resp7,resp8,resp9,stla,stlo,stel,stdp,evla,evlo,evel,evdp,mag,user0,user1,   &
-user2,user3,user4,user5,user6,user7,user8,user9,dist,az,baz,gcarc,internal2, &
-internal3,depmen,cmpaz,cmpinc,xminimum,xmaximum,yminimum,ymaximum,unused1,   &
-unused2,unused3,unused4,unused5,unused6,unused7,nzyear,nzjday,nzhour,nzmin,  &
-nzsec,nzmsec,nvhdr,norid,nevid,npts,internal4,nwfid,nxsize,nysize,unused8,   &
-iftype,idep,iztype,unused9,iinst,istreg,ievreg,ievtyp,iqual,isynth,imagtyp,  &
-imagsrc,unused10,unused11,unused12,unused13,unused14,unused15,unused16,      &
-unused17,leven,lpspol,lovrok,lcalda,unused18,kevnm,kstnm,khole,ko,ka,kt0,kt1,&
-kt2,kt3,kt4,kt5,kt6,kt7,kt8,kt9,kf,kuser0,kuser1,kuser2,kcmpnm,knetwk,kdatrd,&
-kinst,sacdata)
+  CALL wbsac(ofile,delta,depmin,depmax,scale,odelta,b,e,o,a,internal1,         &
+  t0,t1,t2,t3,t4,t5,t6,t7,t8,t9,f,resp0,resp1,resp2,resp3,resp4,resp5,resp6,   &
+  resp7,resp8,resp9,stla,stlo,stel,stdp,evla,evlo,evel,evdp,mag,user0,user1,   &
+  user2,user3,user4,user5,user6,user7,user8,user9,dist,az,baz,gcarc,internal2, &
+  internal3,depmen,cmpaz,cmpinc,xminimum,xmaximum,yminimum,ymaximum,unused1,   &
+  unused2,unused3,unused4,unused5,unused6,unused7,nzyear,nzjday,nzhour,nzmin,  &
+  nzsec,nzmsec,nvhdr,norid,nevid,npts,internal4,nwfid,nxsize,nysize,unused8,   &
+  iftype,idep,iztype,unused9,iinst,istreg,ievreg,ievtyp,iqual,isynth,imagtyp,  &
+  imagsrc,unused10,unused11,unused12,unused13,unused14,unused15,unused16,      &
+  unused17,leven,lpspol,lovrok,lcalda,unused18,kevnm,kstnm,khole,ko,ka,kt0,kt1,&
+  kt2,kt3,kt4,kt5,kt6,kt7,kt8,kt9,kf,kuser0,kuser1,kuser2,kcmpnm,knetwk,kdatrd,&
+  kinst,sacdata)
 
 
   !Deallocate for next file
@@ -204,125 +222,129 @@ ENDDO
 
 END PROGRAM noisy
 
-  !COOL
-  !*----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*
-  !Subroutine modifed from Numerical Recipes in Fortran
-  SUBROUTINE cool(data,nsize,nn,ndim,isign)
-  INTEGER(KIND=4), INTENT(IN) :: isign, ndim, nsize
-  INTEGER(KIND=4), DIMENSION(ndim), INTENT(IN) :: nn
-  REAL(KIND=4), DIMENSION(nsize), INTENT(INOUT) :: data
-  INTEGER(KIND=4) :: i1,i2,i2rev,i3,i3rev,ibit,idim,ifp1,ifp2,ip1,ip2,ip3,k1, &
-                     k2,n,nprev,nrem,ntot
-  REAL(KIND=4) :: tempi,tempr
-  REAL(KIND=8) :: theta,wi,wpi,wpr,wr,wtemp
-  ntot=1
-  DO idim=1,ndim
-    ntot=ntot*nn(idim)
-  ENDDO
-  nprev=1
-  DO idim=1,ndim
-    n=nn(idim)
-    nrem=ntot/(n*nprev)
-    ip1=2*nprev
-    ip2=ip1*n
-    ip3=ip2*nrem
-    i2rev=1
-    DO i2=1,ip2,ip1
-      IF (i2 < i2rev) THEN
-        DO i1=i2,i2+ip1-2,2
-          DO i3=i1,ip3,ip2
-            i3rev=i2rev+i3-i2
-            tempr=data(i3)
-            tempi=data(i3+1)
-            data(i3)=data(i3rev)
-            data(i3+1)=data(i3rev+1)
-            data(i3rev)=tempr
-            data(i3rev+1)=tempi
-          ENDDO
+!:=====:=====:=====:=====:=====:=====:=====:=====:=====:=====:=====:=====:=====:!
+! SUBROUTINES
+!:=====:=====:=====:=====:=====:=====:=====:=====:=====:=====:=====:=====:=====:!
+
+!COOL
+!*----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*
+!Subroutine modifed from Numerical Recipes in Fortran
+SUBROUTINE cool(data,nsize,nn,ndim,isign)
+INTEGER(KIND=4), INTENT(IN) :: isign, ndim, nsize
+INTEGER(KIND=4), DIMENSION(ndim), INTENT(IN) :: nn
+REAL(KIND=4), DIMENSION(nsize), INTENT(INOUT) :: data
+INTEGER(KIND=4) :: i1,i2,i2rev,i3,i3rev,ibit,idim,ifp1,ifp2,ip1,ip2,ip3,k1, &
+                   k2,n,nprev,nrem,ntot
+REAL(KIND=4) :: tempi,tempr
+REAL(KIND=8) :: theta,wi,wpi,wpr,wr,wtemp
+ntot=1
+DO idim=1,ndim
+  ntot=ntot*nn(idim)
+ENDDO
+nprev=1
+DO idim=1,ndim
+  n=nn(idim)
+  nrem=ntot/(n*nprev)
+  ip1=2*nprev
+  ip2=ip1*n
+  ip3=ip2*nrem
+  i2rev=1
+  DO i2=1,ip2,ip1
+    IF (i2 < i2rev) THEN
+      DO i1=i2,i2+ip1-2,2
+        DO i3=i1,ip3,ip2
+          i3rev=i2rev+i3-i2
+          tempr=data(i3)
+          tempi=data(i3+1)
+          data(i3)=data(i3rev)
+          data(i3+1)=data(i3rev+1)
+          data(i3rev)=tempr
+          data(i3rev+1)=tempi
         ENDDO
-      ENDIF
-      ibit=ip2/2
-      DO WHILE ((ibit >= ip1).AND.(i2rev > ibit))
-        i2rev=i2rev-ibit
-        ibit=ibit/2
       ENDDO
-      i2rev=i2rev+ibit
+    ENDIF
+    ibit=ip2/2
+    DO WHILE ((ibit >= ip1).AND.(i2rev > ibit))
+      i2rev=i2rev-ibit
+      ibit=ibit/2
     ENDDO
-    ifp1=ip1
-    DO WHILE (ifp1 < ip2)
-      ifp2=2*ifp1
-      theta=isign*6.28318530717959_8/(ifp2/ip1)
-      wpr=-2.0_8*sin(0.5_8*theta)**2
-      wpi=sin(theta)
-      wr=1.0_8
-      wi=0.0_8
-      DO i3=1,ifp1,ip1
-        DO i1=i3,i3+ip1-2,2
-          DO i2=i1,ip3,ifp2
-            k1=i2
-            k2=k1+ifp1
-            tempr=sngl(wr)*data(k2)-sngl(wi)*data(k2+1)
-            tempi=sngl(wr)*data(k2+1)+sngl(wi)*data(k2)
-            data(k2)=data(k1)-tempr
-            data(k2+1)=data(k1+1)-tempi
-            data(k1)=data(k1)+tempr
-            data(k1+1)=data(k1+1)+tempi
-          ENDDO
+    i2rev=i2rev+ibit
+  ENDDO
+  ifp1=ip1
+  DO WHILE (ifp1 < ip2)
+    ifp2=2*ifp1
+    theta=isign*6.28318530717959_8/(ifp2/ip1)
+    wpr=-2.0_8*sin(0.5_8*theta)**2
+    wpi=sin(theta)
+    wr=1.0_8
+    wi=0.0_8
+    DO i3=1,ifp1,ip1
+      DO i1=i3,i3+ip1-2,2
+        DO i2=i1,ip3,ifp2
+          k1=i2
+          k2=k1+ifp1
+          tempr=sngl(wr)*data(k2)-sngl(wi)*data(k2+1)
+          tempi=sngl(wr)*data(k2+1)+sngl(wi)*data(k2)
+          data(k2)=data(k1)-tempr
+          data(k2+1)=data(k1+1)-tempi
+          data(k1)=data(k1)+tempr
+          data(k1+1)=data(k1+1)+tempi
         ENDDO
-        wtemp=wr
-        wr=wr*wpr-wi*wpi+wr
-        wi=wi*wpr+wtemp*wpi+wi
       ENDDO
-      ifp1=ifp2
+      wtemp=wr
+      wr=wr*wpr-wi*wpi+wr
+      wi=wi*wpr+wtemp*wpi+wi
     ENDDO
-    nprev=n*nprev
+    ifp1=ifp2
   ENDDO
+  nprev=n*nprev
+ENDDO
 
-  END SUBROUTINE cool
-  !*----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*
+END SUBROUTINE cool
+!*----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*
 
-  !MOMENT
-  !*----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*
-  !Subroutine modifed from Numerical Recipes in Fortran
+!MOMENT
+!*----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*
+!Subroutine modifed from Numerical Recipes in Fortran
 
-  SUBROUTINE moment(data,n,ave,adev,sdev,var,skew,curt)
-  INTEGER(KIND=4), INTENT(IN) :: n
-  REAL(KIND=4), DIMENSION(n), INTENT(IN) :: data
-  REAL(KIND=4), INTENT(OUT) :: adev,ave,curt,sdev,skew,var
-  REAL(KIND=4) :: p, s, ep
-  INTEGER(KIND=4) :: j
+SUBROUTINE moment(data,n,ave,adev,sdev,var,skew,curt)
+INTEGER(KIND=4), INTENT(IN) :: n
+REAL(KIND=4), DIMENSION(n), INTENT(IN) :: data
+REAL(KIND=4), INTENT(OUT) :: adev,ave,curt,sdev,skew,var
+REAL(KIND=4) :: p, s, ep
+INTEGER(KIND=4) :: j
 
-  s=0.
-  DO j=1,n
-    s=s+data(j)
-  ENDDO
-  ave=s/n
-  adev=0.
-  var=0.
-  skew=0.
-  curt=0.
-  ep=0.
-  DO j=1,n
-    s=data(j)-ave
-    ep=ep+s
-    adev=adev+abs(s)
-    p=s*s
-    var=var+p
-    p=p*s
-    skew=skew+p
-    p=p*s
-    curt=curt+p
-  ENDDO
-  adev=adev/n
-  var=(var-ep**2/n)/(n-1)
-  sdev=sqrt(var)
-  IF (var.ne.0.) THEN
-    skew=skew/(n*sdev**3)
-    curt=curt/(n*var**2)-3.
-  ELSE
-  !      pause 'no skew or kurtosis when zero variance in moment'
-  ENDIF
+s=0.
+DO j=1,n
+  s=s+data(j)
+ENDDO
+ave=s/n
+adev=0.
+var=0.
+skew=0.
+curt=0.
+ep=0.
+DO j=1,n
+  s=data(j)-ave
+  ep=ep+s
+  adev=adev+abs(s)
+  p=s*s
+  var=var+p
+  p=p*s
+  skew=skew+p
+  p=p*s
+  curt=curt+p
+ENDDO
+adev=adev/n
+var=(var-ep**2/n)/(n-1)
+sdev=sqrt(var)
+IF (var.ne.0.) THEN
+  skew=skew/(n*sdev**3)
+  curt=curt/(n*var**2)-3.
+ELSE
+!      pause 'no skew or kurtosis when zero variance in moment'
+ENDIF
 
-  END SUBROUTINE moment
-  !*----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*
+END SUBROUTINE moment
+!*----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*
 
---
+++ examples/Run_Examples_06.csh
@@ -0,0 +1,85 @@
+#!/bin/csh
+
+#---------------------------------------------------------------------------------------#
+# Script for doing some basic testing of routines.
+# Plotting is done using the Generic Mapping Tools (GMT) package
+#
+# This script tests:
+# addnoise
+#
+#---------------------------------------------------------------------------------------#
+
+
+# Test addnoise
+#---------------------------------------------------------------------------------------#
+set RMS = 0.1
+set corner = 0.4
+
+
+ls DDP*.T >! flist
+../bin/addnoise flist $RMS $corner
+
+# first lets look at an example of the noise generated
+../bin/sac2xy noise.sac noise.xy
+
+psxy noise.xy -JX6i/1.75i -Y1.25i -R1160/1260/-0.5/1.1 -P -K -W4/0/0/205 \
+  -B20g10000f5:"Time (s)":/1g10000f.1nSeW >! Examples_06.ps
+pstext -JX -R -N -O -K -G0/0/205 << eof >> Examples_06.ps
+1160 1.2 12 0 1 1 'noise.sac'
+eof
+pstext -JX -R -N -O -K -G205/0/0 << eof >> Examples_06.ps
+1162 0.9 11 0 1 1 RMS: $RMS
+1162 0.75 11 0 1 1 Corner: $corner (s)
+eof
+
+# Now plot seismogram and seismogram with noise added
+../bin/sac2xy DDP_h300_s2.0_210.T A.xy
+../bin/sac2xy noise_DDP_h300_s2.0_210.T B.xy
+
+psxy A.xy -JX -Y2.25i -R1160/1260/-0.5/1.1 -P -K -O -W4/0/0/205 -B20g10000f5/1g10000f.1nseW  >> Examples_06.ps
+pstext -JX -R -N -O -K -G0/0/205 << eof >> Examples_06.ps
+1160 1.2 12 0 1 1 Synthetic: DDP_h300_s2.0_210.T
+eof
+
+
+psxy B.xy -JX -Y2.25i -R1160/1260/-0.5/1.1 -P -K -O -W4/0/0/205 -B >> Examples_06.ps
+pstext -JX -R -N -O -K -G0/0/205 << eof >> Examples_06.ps
+1160 1.2 12 0 1 1 Synthetic + Noise
+eof
+
+
+# FFT on noise
+sac << efg
+r noise.sac
+fft 
+writesp 
+q
+efg
+
+# plot amplitude spectrum as a function of period
+../bin/sac2xy noise.sac.am A.xy
+awk 'NR > 1 {print (1.0/$1), $2}' A.xy >! B.xy
+psxy B.xy -JX6il/2il -Y2.75i -R0.01/1000/0.01/10 -W1/0/0/205 -P -O -K \
+  -B10g10000:"Period (s)":/10g10000nSeW >> Examples_06.ps
+
+# add true corner (2*pi*$corner)
+set cc = `awk 'NR == 1 {print (2*3.141592654*'$corner')}' A.xy`
+psxy -JX -R -W2/205/0/0 -P -O -K << eof >> Examples_06.ps
+$cc .01
+$cc 10
+eof
+
+pstext -JX -R -N -O -G0/0/205 << eof >> Examples_06.ps
+0.01 12 12 0 1 1 Amplitude Spectrum of noise
+eof
+
+
+rm flist
+rm noise.sac noise.xy
+rm noise.sac.am noise.sac.ph
+rm A.xy B.xy
+rm auto_gauss.xy
+rm noise_DDP_h300_s2.0_210.T
+rm .gmtcommands4
+
+gs -sDEVICE=x11 Examples_06.ps
